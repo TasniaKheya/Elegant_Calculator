a346de2cd3fa48c53218c4566275aa41a98926de
TasniaKheya <23kheya@gmail.com>
2022-05-14 11:53:15 +0600

    java class added

---
+++ app/src/main/java/GIT/git_assign.java
@@ -0,0 +1,28 @@
+package GIT;
+
+public class git_assign {
+
+    int roll;
+    String name;
+
+    public git_assign(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
